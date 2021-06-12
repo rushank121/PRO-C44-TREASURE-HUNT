ae8e9186dcd0ca3d4ff862513509ab89bf19a4a3
rushank121 <79090902+rushank121@users.noreply.github.com>
2021-06-12 12:37:27 +0530

    Add files via upload

---
+++ Treasure-Hunt-master/js/System.js
@@ -0,0 +1,12 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        if(actualCode === enteredCode);
+            return true
+       //else 
+         //   return false
+    }
+
+}
